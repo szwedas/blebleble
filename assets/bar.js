8cbba6b854bbf42839fa67d4ff437383df6473d0
fistaszjo <pisztaszek@gmail.com>
2025-05-01 22:00:30 +0200

    Update bar.js

--- assets/bar.js
+++ assets/bar.js
@@ -2,7 +2,7 @@
 var params = new URLSearchParams(window.location.search);
 
 function sendTo(url){
-    location.href = `/${url}?` + params;
+    location.href = `/FistaszjoObywatel/${url}?` + params;
 }
 
 document.querySelectorAll(".bottom_element_grid").forEach((element) => {
